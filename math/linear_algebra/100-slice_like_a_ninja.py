e33239669ed8147c88ac11576ba41e5d79ecf87d
Justine UMUHOZA <u.justine@alustudent.com>
2025-09-22 01:51:57 -0700

    100-slice_like_a_ninja.py

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -3,8 +3,6 @@
 Slice Like A Ninja
 """
 
-import numpy as np
-
 
 def np_slice(matrix, axes={}):
     """
@@ -16,13 +14,12 @@ def np_slice(matrix, axes={}):
     Returns:
         A new numpy.ndarray sliced accordingly
     """
-    # Create a list of slices (one for each dimension)
+    # Create a list of slices (one for each dimension of the matrix)
     slices = [slice(None)] * matrix.ndim
 
     # Replace slices for the axes provided
     for axis, s in axes.items():
         slices[axis] = slice(*s)
 
-    # Apply the slicing
+    # Apply slicing
     return matrix[tuple(slices)]
-
