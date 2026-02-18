30f8e6645da8a21d3a5a3bcfe6d4a12e979f53e3
justine-abc <u.justine@alustudent.com>
2026-02-18 08:37:45 +0200

    edited3-mmain

--- supervised_learning/classification/3-main.py
+++ supervised_learning/classification/3-main.py
@@ -1,47 +1,18 @@
 #!/usr/bin/env python3
-"""Neuron class for binary classification"""
+"""Test 3-neuron.py"""
 import numpy as np
 
+Neuron = __import__('3-neuron').Neuron
 
-class Neuron:
-    """Defines a single neuron performing binary classification"""
+lib_train = np.load('../data/Binary_Train.npz')
+X_3D, Y = lib_train['X'], lib_train['Y']
 
-    def __init__(self, nx):
-        """Initialize the neuron"""
-        if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
-        if nx < 1:
-            raise ValueError("nx must be a positive integer")
+X = X_3D.reshape((X_3D.shape[0], -1)).T
 
-        self.__W = np.random.randn(1, nx)
-        self.__b = 0
-        self.__A = 0
+np.random.seed(0)
+neuron = Neuron(X.shape[0])
 
-    @property
-    def W(self):
-        """Getter for weights"""
-        return self.__W
+A = neuron.forward_prop(X)
+cost = neuron.cost(Y, A)
 
-    @property
-    def b(self):
-        """Getter for bias"""
-        return self.__b
-
-    @property
-    def A(self):
-        """Getter for activated output"""
-        return self.__A
-
-    def forward_prop(self, X):
-        """Calculates forward propagation"""
-        Z = np.matmul(self.__W, X) + self.__b
-        self.__A = 1 / (1 + np.exp(-Z))
-        return self.__A
-
-    def cost(self, Y, A):
-        """Calculates cost using logistic regression loss"""
-        m = Y.shape[1]
-        cost = - (1 / m) * np.sum(
-            Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
-        )
-        return cost
+print(cost)
