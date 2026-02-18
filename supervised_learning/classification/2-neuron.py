8e16fdbc1986aa976dc3f69fde208b96e5dcaed8
justine-abc <u.justine@alustudent.com>
2026-02-18 08:49:19 +0200

    edited2-neuron"

--- supervised_learning/classification/2-neuron.py
+++ supervised_learning/classification/2-neuron.py
@@ -1,14 +1,39 @@
 #!/usr/bin/env python3
-"""Neuron class with forward propagation"""
+"""Neuron class for binary classification"""
 import numpy as np
-from 1-neuron import Neuron
 
 
-class Neuron(Neuron):
-    """Neuron with forward propagation"""
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Initialize the neuron"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for weights"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for activated output"""
+        return self.__A
 
     def forward_prop(self, X):
-        """Calculates forward propagation using sigmoid"""
-        z = np.dot(self.W, X) + self.b
-        self._Neuron__A = 1 / (1 + np.exp(-z))
-        return self.A
+        """Calculates forward propagation"""
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
