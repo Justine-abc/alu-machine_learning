50a14e1fe3241e2246adf8602dda838f4c9d0484
justine-abc <u.justine@alustudent.com>
2026-02-19 18:00:11 +0200

    1-gp

---
+++ unsupervised_learning/hyperparameter_tuning/1-gp.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Gaussian Process with prediction"""
+
+import numpy as np
+
+
+class GaussianProcess:
+    """Noiseless 1D Gaussian Process"""
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(self.X, self.X)
+
+    def kernel(self, X1, X2):
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) + \
+                 np.sum(X2**2, 1) - 2 * np.matmul(X1, X2.T)
+
+        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
+
+    def predict(self, X_s):
+        """Predict mean and variance"""
+        K_s = self.kernel(self.X, X_s)
+        K_ss = self.kernel(X_s, X_s)
+        K_inv = np.linalg.inv(self.K)
+
+        mu = K_s.T @ K_inv @ self.Y
+        mu = mu.reshape(-1)
+
+        cov = K_ss - K_s.T @ K_inv @ K_s
+        sigma = np.diag(cov)
+
+        return mu, sigma
