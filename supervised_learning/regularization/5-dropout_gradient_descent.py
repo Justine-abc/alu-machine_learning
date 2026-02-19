9c43420cb4ac3e8c19f3840f04462ec054e0aa49
justine-abc <u.justine@alustudent.com>
2026-02-19 11:26:40 +0200

    edited5

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -6,24 +6,25 @@ import numpy as np
 def dropout_gradient_descent(Y, weights, cache,
                              alpha, keep_prob, L):
     """
-    Updates weights using gradient descent with dropout
+    Updates the weights of a neural network
+    using gradient descent with Dropout regularization
     """
     m = Y.shape[1]
     dZ = cache["A" + str(L)] - Y
 
     for layer in reversed(range(1, L + 1)):
         A_prev = cache["A" + str(layer - 1)]
-        W = weights["W" + str(layer)]
+        W_current = weights["W" + str(layer)]
 
         dW = (1 / m) * np.matmul(dZ, A_prev.T)
         db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
-        weights["W" + str(layer)] -= alpha * dW
-        weights["b" + str(layer)] -= alpha * db
-
         if layer > 1:
-            dA_prev = np.matmul(W.T, dZ)
-            D = cache["D" + str(layer - 1)]
-            dA_prev = (dA_prev * D) / keep_prob
+            dA_prev = np.matmul(W_current.T, dZ)
+            D_prev = cache["D" + str(layer - 1)]
+            dA_prev = (dA_prev * D_prev) / keep_prob
             A_prev = cache["A" + str(layer - 1)]
             dZ = dA_prev * (1 - np.power(A_prev, 2))
+
+        weights["W" + str(layer)] -= alpha * dW
+        weights["b" + str(layer)] -= alpha * db
