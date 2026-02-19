368849c5239042dc4b451345c714f1b9e7e2eb44
justine-abc <u.justine@alustudent.com>
2026-02-19 18:40:03 +0200

    edited5

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -1,35 +1,27 @@
-#!/usr/bin/env python3
-"""Bayesian Optimization: Optimization Loop"""
+def optimize(self, iterations=100):
+    """
+    Optimizes the black-box function.
 
-import numpy as np
-BO_base = __import__('4-bayes_opt').BayesianOptimization
+    iterations: max number of iterations
 
+    Returns:
+    X_opt: numpy.ndarray of shape (1,)
+    Y_opt: numpy.ndarray of shape (1,)
+    """
+    for _ in range(iterations):
+        X_next, _ = self.acquisition()
+        # Stop if X_next has already been sampled
+        if np.any(np.isclose(X_next, self.gp.X, atol=1e-8)):
+            break
 
-class BayesianOptimization(BO_base):
-    """Extends BayesianOptimization with an optimize() method"""
+        Y_next = self.f(X_next)
+        self.gp.update(X_next, Y_next)
 
-    def optimize(self, iterations=100):
-        """
-        Optimizes the black-box function.
+    if self.minimize:
+        idx = np.argmin(self.gp.Y)
+    else:
+        idx = np.argmax(self.gp.Y)
 
-        iterations: max number of iterations
-
-        Returns:
-        X_opt: numpy.ndarray of shape (1,)
-        Y_opt: numpy.ndarray of shape (1,)
-        """
-        for _ in range(iterations):
-            X_next, _ = self.acquisition()
-            if np.any(np.isclose(X_next, self.gp.X)):
-                break
-            Y_next = self.f(X_next)
-            self.gp.update(X_next, Y_next)
-
-        if self.minimize:
-            idx = np.argmin(self.gp.Y)
-        else:
-            idx = np.argmax(self.gp.Y)
-
-        X_opt = self.gp.X[idx].reshape(1,)
-        Y_opt = self.gp.Y[idx].reshape(1,)
-        return X_opt, Y_opt
+    X_opt = self.gp.X[idx].reshape(1,)
+    Y_opt = self.gp.Y[idx].reshape(1,)
+    return X_opt, Y_opt
