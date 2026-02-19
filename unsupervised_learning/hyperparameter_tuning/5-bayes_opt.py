dbf378798ed6e58f9bbb7665bb362cf4e4eb9929
justine-abc <u.justine@alustudent.com>
2026-02-19 19:36:10 +0200

    edited==5

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -10,7 +10,7 @@ class BayesianOptimization(BO_base):
 
     def optimize(self, iterations=100):
         """
-        Optimizes the black-box function.
+        Optimizes the black-box function using Expected Improvement.
 
         iterations: max number of iterations
 
@@ -19,14 +19,20 @@ class BayesianOptimization(BO_base):
         Y_opt: numpy.ndarray of shape (1,)
         """
         for _ in range(iterations):
+            # Get next best sample from acquisition
             X_next, _ = self.acquisition()
+
             # Stop if X_next has already been sampled
             if np.any(np.isclose(X_next, self.gp.X, atol=1e-8)):
                 break
 
+            # Sample the function at X_next
             Y_next = self.f(X_next)
+
+            # Update the Gaussian process with the new point
             self.gp.update(X_next, Y_next)
 
+        # Determine optimal point and value
         if self.minimize:
             idx = np.argmin(self.gp.Y)
         else:
