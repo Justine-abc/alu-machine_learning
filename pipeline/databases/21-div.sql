9392bc8d86964a9198ea372d0f9d5f029f7c58ad
Justine UMUHOZA <u.justine@alustudent.com>
2026-03-23 09:43:22 -0700

    21-div.sql

---
+++ pipeline/databases/21-div.sql
@@ -0,0 +1,12 @@
+-- Creates function SafeDiv to handle division by zero
+DELIMITER //
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+    RETURN a / b;
+END;
+//
+DELIMITER ;
